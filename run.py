ba5aeecd0f8444f921d4910ad175c3403b014ed2
Rudy <rudy.garcia@ug.uchile.cl>
2022-07-26 12:21:08 -0400

    Initial codebase

---
+++ run.py
@@ -0,0 +1,40 @@
+import d3rlpy
+from d3rlpy.datasets import get_cartpole
+import numpy as np
+import pickle5 as pickle    # noqa
+import os
+
+# so far we have needed: pickle5, stable-baselines3, matplotlib
+
+with open(os.getenv("RB_PATH"), "rb") as f:
+    sb3_buffer = pickle.load(f, fix_imports=True)
+
+dataset = d3rlpy.dataset.MDPDataset(
+    observations=sb3_buffer.observations.squeeze(1),
+    actions=sb3_buffer.actions.squeeze(1),
+    rewards=sb3_buffer.rewards.squeeze(1),
+    terminals=sb3_buffer.dones.squeeze(1),
+)
+
+
+from d3rlpy.algos import CQL
+from d3rlpy.metrics.scorer import discounted_sum_of_advantage_scorer
+from d3rlpy.metrics.scorer import td_error_scorer
+from d3rlpy.metrics.scorer import average_value_estimation_scorer
+from sklearn.model_selection import train_test_split
+
+# setup CQL algorithm
+cql = CQL(use_gpu=False)
+
+# split train and test episodes
+train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
+
+# start training
+cql.fit(train_episodes,
+        eval_episodes=test_episodes,
+        n_epochs=2,
+        scorers={
+            'advantage': discounted_sum_of_advantage_scorer, # smaller is better
+            'td_error': td_error_scorer, # smaller is better
+            'value_scale': average_value_estimation_scorer # smaller is better
+        })
---
+++ train.bash
@@ -0,0 +1,2 @@
+ #!/bin/bash
+ docker run -v $(pwd):/workspace -e RB_PATH=/workspace/data/random_buffer.pkl -it lhdorl
